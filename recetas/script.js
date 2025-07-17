8241ee3daa8753594106d9428e620801d3e9bf01
Pablo Acevedo Areco <16298855+pabloacvd@users.noreply.github.com>
2025-07-17 09:08:11 +0200

    Update script.js

--- recetas/script.js
+++ recetas/script.js
@@ -86,7 +86,6 @@ function parseRecipe(content, filename) {
                 line.includes('.jpg') || line.includes('.png') || line.includes('.jpeg') || 
                 line.includes('.gif') || line.includes('.webp')) {
                 images.push(line);
-                console.log("Agrego 1 img");
             }
         }
     }
